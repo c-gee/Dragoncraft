4e52ce5ee3e875ad451f0cdd459b2487ebba6266
Choon Gee <cgee.code@gmail.com>
2024-05-07 01:31:41 +0800

    [Chapter6] Selecting the units

--- Assets/Scripts/Unit/UnitComponent.cs
+++ Assets/Scripts/Unit/UnitComponent.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 namespace Dragoncraft
 {
+    [RequireComponent(typeof(BoxCollider), typeof(Animator))]
     public class UnitComponent : MonoBehaviour
     {
         public string ID;
@@ -13,5 +15,54 @@ namespace Dragoncraft
         public float Defense;
         public float WalkSpeed;
         public float AttackSpeed;
+        public Color SelectedColor;
+
+        private Animator _animator;
+        private Renderer _renderer;
+
+        private void Awake()
+        {
+            _renderer = GetComponentInChildren<Renderer>();
+            _animator = GetComponent<Animator>();
+            _animator.Play("Idle");
+        }
+
+        public void CopyData(UnitData unitData)
+        {
+            ID = Guid.NewGuid().ToString();
+            Type = unitData.Type;
+            Level = unitData.Level;
+            LevelMultiplier = unitData.LevelMultiplier;
+            Health = unitData.Health;
+            Attack = unitData.Attack;
+            Defense = unitData.Defense;
+            WalkSpeed = unitData.WalkSpeed;
+            AttackSpeed = unitData.AttackSpeed;
+            SelectedColor = unitData.SelectedColor;
+        }
+
+        public void Selected(bool selected)
+        {
+            if (_renderer == null)
+            {
+                Debug.LogError("Renderer component is missing!");
+                return;
+            }
+
+            Material[] materials = _renderer.materials;
+
+            foreach (Material material in materials)
+            {
+                if (selected)
+                {
+                    material.EnableKeyword("_EMISSION");
+                    material.SetColor("_EmissionColor", SelectedColor * 0.5f);
+                }
+                else
+                {
+                    material.DisableKeyword("_EMISSION");
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Unit/UnitSelectorComponent.cs
+++ Assets/Scripts/Unit/UnitSelectorComponent.cs
@@ -1,18 +1,81 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitSelectorComponent : MonoBehaviour
+namespace Dragoncraft
 {
-    // Start is called before the first frame update
-    void Start()
+    public class UnitSelectorComponent : MonoBehaviour
     {
-        
-    }
+        private MeshCollider _meshCollider = null;
+        private Vector3 _startPosition;
+        private List<UnitComponent> _units = new List<UnitComponent>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            GameObject plane = GameObject.FindGameObjectWithTag("Plane");
+
+            if (plane != null)
+            {
+                _meshCollider = plane.GetComponent<MeshCollider>();
+            }
+
+            if (_meshCollider == null)
+            {
+                Debug.LogError("Missing tag and/or MeshCollider reference!");
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                _startPosition = GetMousePosition();
+            }
+
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                Vector3 endPosition = GetMousePosition();
+                SelectUnits(_startPosition, endPosition);
+            }
+        }
+
+        private Vector3 GetMousePosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitData;
+
+            if (_meshCollider.Raycast(ray, out hitData, 1000))
+            {
+                return hitData.point;
+            }
+
+            return Vector3.zero;
+        }
+
+        private void SelectUnits(Vector3 startPosition, Vector3 endPosition)
+        {
+            foreach (UnitComponent unit in _units)
+            {
+                unit.Selected(false);
+            }
+
+            _units.Clear();
+
+            Vector3 center = (startPosition + endPosition) / 2;
+            float distance = Vector3.Distance(center, endPosition);
+            Vector3 halfExtents = new Vector3(distance, distance, distance);
+
+            Collider[] colliders = Physics.OverlapBox(center, halfExtents);
+
+            foreach (Collider collider in colliders)
+            {
+                UnitComponent unit = collider.GetComponent<UnitComponent>();
+
+                if (unit != null)
+                {
+                    unit.Selected(true);
+                    _units.Add(unit);
+                }
+            }
+        }
     }
 }
